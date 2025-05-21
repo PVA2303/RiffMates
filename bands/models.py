6058a2d881ccf57056d6b725fc3555f384ad20fb
PVA2303 <polinagranger88@gmail.com>
2025-05-21 17:45:49 +0700

    many to many

--- bands/models.py
+++ bands/models.py
@@ -7,3 +7,20 @@ class Musician(models.Model):
     birth = models.DateField()
     def __srt__(self):
         return f"Musician(id={self.id}, last_name={self.last_name})"
+
+class Band(models.Model):
+    name = models.CharField(max_length=20)
+    musicians = models.ManyToManyField(Musician)
+    def __str__(self):
+        return f"Band(id={self.id}, name={self.name})"
+
+class Venue(models.Model):
+    name = models.CharField(max_length=20)
+    def __str__(self):
+        return f"Venue(id={self.id}, name={self.name})"
+
+class Room(models.Model):
+    name = models.CharField(max_length=20)
+    venue = models.ForeignKey(Venue, on_delete=models.CASCADE)
+    def __str__(self):
+        return f"Room(id={self.id}, name={self.name})"
--- bands/urls.py
+++ bands/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path("musician/<int:musician_id>", views.musician, name="musician"),
     path("musician/<int:musician_id>", views.musician, name="musician"),
     path("musicians/", views.musicians, name="musicians"),
+    path('bands/', views.bands_list, name='bands'),
+    path('bands/<int:pk>/', views.band_detail, name='band'),
 ]
